330e12b03507a33e2a0e2c46fd4d7e207620356a
sayali surve <sayalisansurve@gmail.com>
2022-03-20 12:05:20 +0530

    updating

--- src/db/conn_mongodb.js
+++ src/db/conn_mongodb.js
@@ -1,7 +1,8 @@
 const mongoose=require('mongoose');
 
 //creating database
-mongoose.connect("mongodb://localhost:27017/employee",{
+// mongoose.connect("mongodb://localhost:27017/employee",{
+mongoose.connect("mongodb+srv://sayali:1234@newclustor.gabyf.mongodb.net/test/employee",{
     // useCreateIndex:true,
     useNewUrlParser:true,
     useUnifiedTopology:true
