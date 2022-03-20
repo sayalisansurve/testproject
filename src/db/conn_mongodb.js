ca937a03e2ff4af87d41ee27eb3f3fb726c7a50f
sayali surve <sayalisansurve@gmail.com>
2022-03-20 15:17:39 +0530

    details

--- src/db/conn_mongodb.js
+++ src/db/conn_mongodb.js
@@ -2,8 +2,8 @@ const mongoose=require('mongoose');
 require("dotenv").config();
 //creating database
 // mongoose.connect("mongodb://localhost:27017/employee",{
-// mongoose.connect("mongodb+srv://sayali:1234@newclustor.gabyf.mongodb.net/employee?retryWrites=true&w=majority",{
-mongoose.connect(process.env.MONGODB_URL,{
+mongoose.connect("mongodb+srv://sayali:1234@newclustor.gabyf.mongodb.net/employee?retryWrites=true&w=majority",{
+// mongoose.connect(process.env.MONGODB_URL,{
     // useCreateIndex:true,
     useNewUrlParser:true,
     useUnifiedTopology:true
